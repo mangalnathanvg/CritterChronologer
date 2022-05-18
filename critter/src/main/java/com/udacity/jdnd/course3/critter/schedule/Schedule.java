145c05ca5ca7aa5a87be180483743a54083b99fa
Mangalnathan Vijayagopal <mangalnathanvg@gmail.com>
2022-05-18 18:49:08 -0400

    Adding entity designs and data layer

---
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/Schedule.java
@@ -0,0 +1,80 @@
+package com.udacity.jdnd.course3.critter.schedule;
+
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import com.udacity.jdnd.course3.critter.user.Employee;
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Table
+@Entity
+public class Schedule {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToMany(targetEntity = Employee.class)
+    private List<Employee> employees;
+
+    @ManyToMany(targetEntity = Pet.class)
+    private List<Pet> pets;
+
+    private LocalDate date;
+
+    @ElementCollection
+    private Set<EmployeeSkill> activities;
+
+    public Schedule() {
+    }
+
+    public Schedule(Long id, List<Employee> employees, List<Pet> pets, LocalDate date, Set<EmployeeSkill> activities) {
+        this.id = id;
+        this.employees = employees;
+        this.pets = pets;
+        this.date = date;
+        this.activities = activities;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Set<EmployeeSkill> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(Set<EmployeeSkill> activities) {
+        this.activities = activities;
+    }
+}
